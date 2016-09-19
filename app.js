7019b8451523e9c6d5e1687ae41bf42e847ed325
Harsha-Nori <harsha.nori@live.com>
2016-09-18 19:56:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -26,6 +26,67 @@ app.get('/', function(req, res) {
     res.send(user.steamID + ': HELLO!');
 });
 
+app.get('/info', function(req, res) {
+	if(!req.query || (!req.query.apps && !req.query.packages)) {
+		sendJsonResponse(req, res, "No apps or packages specified", 400);
+		return;
+	}
+	
+	var apps = req.query.apps ? req.query.apps.split(',') : [];
+	var packages = req.query.packages ? req.query.packages.split(',') : [];
+	
+	if(apps.concat(packages).some(function(id) {
+		return isNaN(id);
+	})) {
+		sendJsonResponse(req, res, "Invalid input ID", 400);
+		return;
+	}
+	
+	apps = apps.map(function(appid) {
+		return parseInt(appid, 10);
+	});
+	
+	packages = packages.map(function(packageid) {
+		return parseInt(packageid, 10);
+	});
+	
+	var timedOut = false;
+	var timeout = setTimeout(function() {
+		timedOut = true;
+		sendJsonResponse(req, res, "Steam request timed out", 504);
+	}, 30000); // 30 seconds
+	
+	user.getProductInfo(apps, packages, function(appData, packageData, unknownApps, unknownPackages) {
+		if(timedOut) {
+			return;
+		}
+		
+		var outApps = {};
+		var outPackages = {};
+		
+		var i;
+		for(i in appData) {
+			if(!appData.hasOwnProperty(i)) {
+				continue;
+			}
+			
+			outApps[i] = appData[i].appinfo;
+			outApps[i].change_number = appData[i].changenumber;
+		}
+		
+		for(i in packageData) {
+			if(!packageData.hasOwnProperty(i)) {
+				continue;
+			}
+			
+			outPackages[i] = packageData[i].packageinfo;
+		}
+		
+		sendJsonResponse(req, res, {"success": 1, "apps": outApps, "packages": outPackages, "unknown_apps": unknownApps, "unknown_packages": unknownPackages});
+		
+		clearTimeout(timeout);
+	});
+});
 
 function cors(req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
@@ -52,6 +113,8 @@ function sendJsonResponse(req, res, response, statusCode) {
     res.set('Content-Type', 'application/json');
     res.send(JSON.stringify(response, null, (req.query && req.query.prettyprint && req.query.prettyprint != 0) ? "\t" : null));
 }
+
+
 
 /*const PORT = 8085;
 const IP = '127.0.0.1';
